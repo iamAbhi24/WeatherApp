e071f9aab7a381c76f9ac066e4889d0696e92c7d
Abhishek <abhisheksrivastava199924@gmail.com>
2023-12-27 17:12:20 +0530

    added cross icon, removing all inputs by clicking on it

--- index.js
+++ index.js
@@ -1,25 +1,37 @@
 // Creating an cross icon inside the span tag dynamically
 
+// Create span Element
 let spanElement=document.createElement('span');
 spanElement.classList.add('crossIcon');
 
-
+// Create i element (font Awesome icon)
 let crossIcon=document.createElement('i');
+crossIcon.classList.add('fa-solid','fa-xmark');
 
-crossIcon.classList.add('fa','fa-times');
 
+// Append the i element to the span element
+spanElement.appendChild(crossIcon);
 let containerElement=document.querySelector('.container');
 
-// Adding cross icon inside the left side of the input text field
 
-let inputValue=document.querySelector('input');
+// Adding cross icon inside the left side of the input text field on input
 function addCrossIcon(){
-    if(inputValue.value==''){
+    containerElement.appendChild(spanElement);
+}
+
+// Remove input after click on cross icon
+crossIcon.addEventListener('click',()=>{
+    document.querySelector('input').value='';
+    containerElement.removeChild(spanElement); //removing the cross icon 
+})
+
+
+document.querySelector('input').addEventListener('blur',()=>{
+    if(document.querySelector('input').value=''){
         containerElement.removeChild(spanElement);
     }
-    else{
-        containerElement.appendChild(spanElement);
-        
-    }
-}
+})
+
+
+
 
