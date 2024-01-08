3263f69d154affe3f0f51dde6ea8eceb98aaa8a9
Abhishek <abhisheksrivastava199924@gmail.com>
2024-01-08 18:06:44 +0530

    Todays weather is done, need to focus  issue with cross icon

--- index.js
+++ index.js
@@ -16,18 +16,18 @@ let containerElement=document.querySelector('.container');
 
 // Adding cross icon inside the right side of the input text field on input and removing it when we remove all inputs from the input filed by backspace button .
 function addCrossIcon(){
-    if(document.querySelector('input').value!==''){
-        containerElement.appendChild(spanElement);
-    }
-    else{
-        containerElement.removeChild(spanElement);
-    }
+   // if(document.querySelector('input').value!==''){
+        // containerElement.appendChild(spanElement);
+   // }
+    // else{
+    //     containerElement.removeChild(spanElement);
+    // }
 }
 
 // Remove input after click on cross icon
 crossIcon.addEventListener('click',()=>{
     document.querySelector('input').value='';
-    containerElement.removeChild(spanElement); //removing the cross icon 
+    // containerElement.removeChild(spanElement); //removing the cross icon 
 })
 
 
@@ -42,7 +42,13 @@ input.addEventListener('keypress', (event)=>{
            fetch(Url)
            .then((response)=>response.json())
            .then((data)=>{
-            document.querySelector('h2').innerHTML=`${data.main.temp}<sup>0</sup>c`;
+            // setting the heading as the name of the city
+           let location = document.createElement('h2');
+            containerElement.appendChild(location);
+            document.querySelector('h2').innerHTML=cityName;
+
+              //diplay weather condtion
+              weatherCondition(data); 
            })
            .catch((error)=>{console.log(error)})
      }
@@ -54,3 +60,30 @@ input.addEventListener('keypress', (event)=>{
 
 
 
+function weatherCondition(data){
+    //creating parent div for displaying todays weather
+    let todayWeather=document.createElement('div');
+    todayWeather.id='today-weather'; //setting id
+    containerElement.appendChild(todayWeather); // append to the container
+    
+    //creating a div inside parent div which will dsiplay todays temperarture
+    let todayTemp=document.createElement('div');
+    todayTemp.id='today-temperature';
+    todayWeather.appendChild(todayTemp); //append to the parent div
+    document.querySelector('#today-temperature').innerHTML=`${data.main.temp}<sup>o</sup>`; //displaying the todays tempertaure
+  
+
+////creating a div inside parent div which will dsiplay current weather condition eg. rain, clear etc.
+    let weatherCondition = document.createElement('div');
+    weatherCondition.id='weather-condition';
+    todayWeather.appendChild(weatherCondition); //append to the parent div
+    document.querySelector('#weather-condition').innerHTML=data.weather[0].description;  //displaying the condition
+    
+    
+ // Creating Div inside the parent div which will display todays lowest and higest temperaature.
+ let minMaxTemp = document.createElement('div');
+ minMaxTemp.id='min-max-temperature';
+ todayWeather.appendChild(minMaxTemp); //append to the parent div
+
+document.querySelector('#min-max-temperature').innerHTML= `L :${data.main.temp_min}<sup>o</sup>   H :${data.main.temp_max}<sup>o</sup>`; // 
+}
